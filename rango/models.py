5de3edc8740e458135f617397aace77a67bdfb29
Yandan Lai <2714855l@student.gla.ac.uk>
2022-02-23 23:46:42 +0000

    chapter5

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,27 @@
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+# Create 2 models, one class representing each model (categories and pages)
+# Both inherit from the Model base class: django.db.models.Model
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
