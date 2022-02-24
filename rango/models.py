822175b9d7149454e6cacfc2bb1855f21860690d
Yandan Lai <2714855l@student.gla.ac.uk>
2022-02-24 13:03:26 +0000

    Chapter6

--- rango/models.py
+++ rango/models.py
@@ -1,15 +1,19 @@
 from tabnanny import verbose
 from django.db import models
-
+from django.template.defaultfilters import slugify
 # Create 2 models, one class representing each model (categories and pages)
 # Both inherit from the Model base class: django.db.models.Model
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-
+    slug = models.SlugField(unique=True)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+    
     class Meta:
         verbose_name_plural = 'Categories'
 
